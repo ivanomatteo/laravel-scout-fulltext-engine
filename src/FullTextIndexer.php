2ebed9a5ef458432c7d74aade512bcf34fa3ce82
ivanomatteo <ivanomatteo@users.noreply.github.com>
2022-08-03 11:29:36 +0000

    Fix styling

--- src/FullTextIndexer.php
+++ src/FullTextIndexer.php
@@ -61,7 +61,7 @@ class FullTextIndexer
 
     private function getDefaultExtractors()
     {
-        if (!isset($this->defaultExtractors)) {
+        if (! isset($this->defaultExtractors)) {
             $this->defaultExtractors = collect(Pkg::configGet('pre_processing.index_data.extractors'))
                 ->map(fn ($extr) => App::make($extr));
         }
@@ -75,7 +75,7 @@ class FullTextIndexer
 
         if (method_exists($model, 'getIndexFeatureExtractors')) {
             $tmp = $model->getIndexFeatureExtractors();
-            if (!empty($tmp)) {
+            if (! empty($tmp)) {
                 $extractors = $tmp;
             }
         }
@@ -145,7 +145,7 @@ class FullTextIndexer
                 if ($col instanceof \Illuminate\Database\Query\Expression) {
                     return $col;
                 }
-                if (!Str::contains($col, '.')) {
+                if (! Str::contains($col, '.')) {
                     return $model->getTable() . '.' . $col;
                 }
 
@@ -205,13 +205,13 @@ class FullTextIndexer
         if ($connection->getDriverName() === 'mysql') {
             $scope = (new MysqlFullTextScope($connection, ['text']))
                 ->search($options['query_prepared']);
-            if (!empty($options['fulltext_options']['mode'])) {
+            if (! empty($options['fulltext_options']['mode'])) {
                 $scope->inBooleanMode();
             }
-            if (!empty($options['fulltext_options']['order_by_score'])) {
+            if (! empty($options['fulltext_options']['order_by_score'])) {
                 $scope->orderByscore();
             }
-            if (!empty($options['fulltext_options']['add_select_score'])) {
+            if (! empty($options['fulltext_options']['add_select_score'])) {
                 $scope->addSelectScore(true);
             }
             $scope->apply($q, $model);
